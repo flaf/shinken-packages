9b59a73806271b9158c61e22638770327cae77af
François Lafont <flafdivers@free.fr>
2014-11-02 01:46:05 +0100

    Update

---
+++ shinken-packs/shinken-packs/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='spnotify',
+    packages=['spnotify'],
+    author='François Lafont',
+    author_email='francois.lafont@ac-versailles.fr',
+    description='Package to send notifications by email, IRC or SMS.',
+    license='GPL-3',
+)
+
