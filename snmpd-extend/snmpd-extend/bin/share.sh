fe79315c6a9c2b75b89f7131cec205ce2850b013
François Lafont <flafdivers@free.fr>
2016-02-14 06:02:16 +0100

    Fix: source with an absolute path

--- snmpd-extend/snmpd-extend/bin/share.sh
+++ snmpd-extend/snmpd-extend/bin/share.sh
@@ -14,8 +14,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-SCRIPT_NAME=${0##*/}
-
 export LC_ALL=C
 export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 
