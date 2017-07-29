e89ae3789b56806f88884d81849ea9cf356ee514
lafont <francois.lafont@ac-versailles.fr>
2017-07-29 03:24:00 +0200

    Add script to test sp_notify

---
+++ shinken-packs/shinken-packs/tests/test_sp_notify.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+script_dir="${0%/*}"
+sp_notify_bin=$(readlink -f "$script_dir/../bin/sp_notify")
+
+blacklist=$(mktemp)
+
+cat > "$blacklist" <<'EOF'
+
+# A comment...
+    # Another comment...
+bad line...
+
+!^lafont$:.*:^reboot$:[00h00;23h59]:[5]
+
+
+EOF
+
+# Just print, no message is sent.
+"$sp_notify_bin" by-email --print-only         \
+    --contact-name='bob'                       \
+    --black-list="$blacklist"                  \
+    --host-name='srv-1'                        \
+    --host-address='172.31.10.10'              \
+    --service-description='reboot'             \
+    --notification-type='PROBLEM'              \
+    --state='CRITICAL'                         \
+    --business-impact='3'                      \
+    --additional-info='The host has rebooted.' \
+    --notification-number='3'                  \
+    --rarefaction-threshold='10'               \
+    --contact-email='bob@dom.tld'              \
+    --contact-number='0676553219'              \
+    --sms-threshold='4'                        \
+    --sms-url='http://foo/sms/'
+
+rm "$blacklist"
+
+
